da14dc35789cc4139ab1f0336fbed72c3e10d085
snasseri-fard <136546535+snasseri-fard@users.noreply.github.com>
2023-06-13 23:30:54 -0400

    Update Lab3.py

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,1 +1,15 @@
+### Lab3: A function that will write a text file to your PC with your name
+
+def write_text_file_with_name():
+    name = input("Enter your name: ")
+
+    # Open the file in write mode
+    with open("output.txt", "w") as file:
+        # Write the name to the file
+        file.write(name)
+
+    print("File created successfully.")
+
+# Example usage:
+write_text_file_with_name()
 
