95c930811d9046b5d1d6bc2feae8dfdfb0cec6dc
Sina Nasseri Fard <snasserifard@gmail.com>
2023-06-14 00:23:36 -0400

    Added new feature to Lab 1 script

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -3,26 +3,26 @@
 from datetime import date
 
 def calculate_age():
-    # Get the current date
-    current_date = date.today()
+    try:
+        current_date = date.today()
 
-    # Prompt the user for their birthdate
-    birth_date = input("Enter your birthdate (YYYY-MM-DD): ")
+        birth_date = input("Enter your birthdate (YYYY-MM-DD): ")
 
-    # Convert the input into a date object
-    birth_date = date.fromisoformat(birth_date)
+        birth_date = date.fromisoformat(birth_date)
 
-    # Calculate the age
-    age = current_date.year - birth_date.year
+        age = current_date.year - birth_date.year
 
-    # Check if the birthday hasn't happened yet this year
-    if (current_date.month, current_date.day) < (birth_date.month, birth_date.day):
-        age -= 1
+        if (current_date.month, current_date.day) < (birth_date.month, birth_date.day):
+            age -= 1
 
-    return age
+        return age
+
+    except TypeError:
+        print("Please enter a valid integer for the birthdate.")
 
 age = calculate_age()
 print("Your age is:", age)
+
 
 def helloWorld():
 	print(‘Hello World’)
